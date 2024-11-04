f5a301a5ab8bb4523e1db69885015d1c86bde4d6
King0fCheeseCats <touterbridge2031@bancroftschool.org>
2024-11-04 12:32:58 -0500

    Nothing

--- helloWorld.groovy
+++ helloWorld.groovy
@@ -1,3 +1,3 @@
 import com.neuronrobotics.bowlerstudio.BowlerKernel
 // Have BowlerStudio speak messages
-BowlerKernel.speak("welcome to bowler studio")
+BowlerKernel.speak("welcome to bowler studio you are great at coding you ugly little troll with a brain as small as a particle he he he he he he he he he he he he he he he he he he he (This is a joke)")
